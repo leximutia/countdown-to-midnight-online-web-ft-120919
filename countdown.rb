6d019573f3eb537e6fac393f0bc7d981cdf739cb
Lexi Mutia <muse.malize@gmail.com>
2019-12-14 17:31:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,30 +1,30 @@
 def countdown(integer)
-	
+
 	count = integer
-	
+
 	while count > 0
 		puts "#{count} SECOND(S)!"
 		count -= 1
 	end
 
 	"HAPPY NEW YEAR!"
-	
+
 end
 
 
 def countdown_with_sleep(integer)
-	
+
 	count = integer
-	
+
 	while count > 0
 
 		puts "#{count} SECOND(S)!"
 		count -= 1
 
 		sleep(1)
-		
+
 	end
 
 	"HAPPY NEW YEAR!"
-	
+
 end
